210fc026896f32a7c6ea5d387083074572275874
Ramon Tayag <ramon.tayag@gmail.com>
2016-11-08 14:27:51 +0800

    Result of `bundle gem dry-validation-matchers`

---
+++ lib/dry/validation/matchers.rb
@@ -0,0 +1,9 @@
+require "dry/validation/matchers/version"
+
+module Dry
+  module Validation
+    module Matchers
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dry/validation/matchers/version.rb
@@ -0,0 +1,7 @@
+module Dry
+  module Validation
+    module Matchers
+      VERSION = "0.1.0"
+    end
+  end
+end
