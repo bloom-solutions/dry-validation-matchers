5a58f478042a9bb4a1a9174b9abaa3b8c93d1ad0
Roland Laurès <roland@rlaures.pro>
2018-05-17 17:15:34 +0200

    Include simplecov to check coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+
+
+# First load and start simplecov
+require 'simplecov'
+SimpleCov.minimum_coverage 90
+SimpleCov.minimum_coverage_by_file 80
+SimpleCov.maximum_coverage_drop 5
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "pry"
 require "pry-byebug"
